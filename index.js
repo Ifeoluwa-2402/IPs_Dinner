833bea6aae18446a4a652cb978af400874697bf1
Ifeoluwa <ifeoluwaolugbode2402@gmail.com>
2026-01-14 16:26:07 +0100

    commit

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import  { menuArray } from './data.js';
 
-console.log(menuArray);
+menuArray.forEach(function(items) {
+    console.log(items.ingredients)
+    document.getElementById('main').innerHTML += `
+    <div class="item">
+        <div class="item-container">
+            <h2 class="item-emoji">${items.emoji}</h2>
+            <div class="item-details">
+                <h3 class="item-name">${items.name}</h3>
+                <p class="item-ingredients">${items.ingredients}</p>
+                <h4 class="item-price">$${items.price}</h4>
+            </div>
+        </div>
+        <button>
+            <i class="fa-solid fa-plus"></i>
+        </button>
+    </div>`
+});
